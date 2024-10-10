8da96c8e48f3e92dbcc609529164ba82295f02a0
rayennnc <rayenchris02@gmail.com>
2024-10-10 11:46:52 +0800

    upload kedua (update)

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { IonicModule } from '@ionic/angular';
+import { IonicModule, LoadingController } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { WeatherService } from '../services/weather.service';
 
@@ -15,29 +15,48 @@ export class HomePage implements OnInit {
   weather: any;
   error: string | null = null;
   cityName: string = 'Manado';
+  currentTime: Date = new Date();
 
-  constructor(private weatherService: WeatherService) {}
+  constructor(
+    private weatherService: WeatherService,
+    private loadingController: LoadingController
+  ) {}
 
   ngOnInit() {
     this.getWeather();
+    setInterval(() => {
+      this.currentTime = new Date();
+    }, 1000);
   }
 
-  getWeather() {
+  async getWeather() {
+    if (!this.cityName.trim()) {
+      this.error = 'Please enter a city name';
+      return;
+    }
+
+    const loading = await this.loadingController.create({
+      message: 'Fetching weather data...',
+      duration: 10000,
+    });
+    await loading.present();
+
     this.weatherService.getWeather(this.cityName).subscribe(
       (data) => {
+        console.log('Received weather data:', data);
         this.weather = data;
         this.error = null;
+        loading.dismiss();
       },
       (err) => {
         console.error('Error fetching weather data', err);
-        this.error = 'Unable to fetch weather data. Please try again later.';
+        this.error = err;
+        loading.dismiss();
       }
     );
   }
 
   searchCity() {
-    if (this.cityName.trim() !== '') {
-      this.getWeather();
-    }
+    this.getWeather();
   }
 }
